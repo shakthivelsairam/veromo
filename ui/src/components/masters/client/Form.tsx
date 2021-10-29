673a99bc3229829f5b8afcb9c42ec98ffc534328
sakthivel <sakthiopr@gmail.com>
2021-10-29 14:04:37 +0530

    Client-Master-changes

--- ui/src/components/masters/client/Form.tsx
+++ ui/src/components/masters/client/Form.tsx
@@ -1,6 +1,40 @@
 import React, {useEffect, useState} from "react";
+import { styled } from '@mui/material/styles';
 import { Route, Link, Switch,withRouter,RouteComponentProps } from "react-router-dom";
-import { Grid, TextField, FormControlLabel,Checkbox,Button,Typography,MenuItem,Select,InputLabel,FormControl,Autocomplete } from '@mui/material';
+import { Tabs, Tab, Box, Grid, TextField, FormControlLabel,Checkbox,Button,Typography,MenuItem,Select,InputLabel,FormControl,Autocomplete } from '@mui/material';
+const Input = styled('input')({
+  display: 'none',
+});
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function PatientForm(props: any){
   const [businessType, setBusinessType] = React.useState('');
@@ -9,6 +43,7 @@ function PatientForm(props: any){
   const [tariffName, setTariffName] = React.useState('');
   const [priceType, setPriceType] = React.useState('');
   const [appliedTo, setAppliedTo] = React.useState('');
+  const [value, setValue] = React.useState(0);
   const locationList = [
     { label: 'Processing Facility', value: "1" },
     { label: 'Collection Facility', value: "2" },
@@ -25,10 +60,22 @@ function PatientForm(props: any){
   ]
   const [selectData, setSelectData] = useState("");
   const [clientType, setClientType] = useState("");
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };  
+
     return(
         <React.Fragment>
+           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+        <Tab label="General" {...a11yProps(0)} />
+          <Tab label="Optional Attributes" {...a11yProps(1)} />
+          <Tab label="Notification" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
         <Grid container spacing={5}>
-          <Grid item xs={true}  style={{marginLeft:1}}>
+        <Grid item xs={3}>
           <FormControl variant="standard">
               <InputLabel id="titles-label">Client Type</InputLabel>
               <Select
@@ -48,6 +95,8 @@ function PatientForm(props: any){
                 <MenuItem value="5">WalkIn</MenuItem>
               </Select>
             </FormControl>
+            </Grid>
+            <Grid item xs={3}>
             <TextField
               id="amount"
               name="amount"
@@ -56,6 +105,8 @@ function PatientForm(props: any){
               variant="standard"
               style={{width:200, marginRight: 15}}
             />
+            </Grid>
+            <Grid item xs={3}>
             <TextField
               id="amount"
               name="amount"
@@ -64,6 +115,8 @@ function PatientForm(props: any){
               variant="standard"
               style={{width:200, marginRight: 15}}
             />
+            </Grid>
+             <Grid item xs={3}>
              <FormControl variant="standard">
               <Autocomplete
               id="testName"
@@ -72,37 +125,15 @@ function PatientForm(props: any){
               renderInput={(params) => <TextField {...params} label="Location" variant="standard" />}
             />
             </FormControl>
-            <FormControl variant="standard">
-              <Autocomplete
-              id="testName"
-              options={hubList}
-              sx={{ width: 200}}
-              renderInput={(params) => <TextField {...params} label="Hub" variant="standard" />}
-            />
-            </FormControl>
+           
             
             
            </Grid>
             
         </Grid>
         <Grid container spacing={5} style={{ paddingTop:10}}>
-        <Grid item xs={true}>
-        <FormControl variant="standard">
-              <Autocomplete
-              id="testName"
-              options={zoneList}
-              sx={{ width: 200,marginRight: 2}}
-              renderInput={(params) => <TextField {...params} label="Zone" variant="standard" />}
-            />
-           </FormControl>
-           <TextField
-              id="amount"
-              name="amount"
-              label="Route"
-              size="small"
-              variant="standard"
-              sx={{ width: 200,marginRight: 2}}
-            />
+        <Grid item xs={3}>
+       
              <TextField
               id="amount"
               name="amount"
@@ -111,6 +142,8 @@ function PatientForm(props: any){
               variant="standard"
               sx={{ width: 200,marginRight: 2}}
             />
+            </Grid>
+            <Grid item xs={3}>
              <TextField
               id="amount"
               name="amount"
@@ -119,6 +152,8 @@ function PatientForm(props: any){
               variant="standard"
               sx={{ width: 200,marginRight: 1}}
             />
+            </Grid>
+            <Grid item xs={3}>
              <FormControl variant="standard">
               <InputLabel id="titles-label">Business Type</InputLabel>
               <Select
@@ -135,19 +170,22 @@ function PatientForm(props: any){
                 <MenuItem value="2">Logistics</MenuItem>
               </Select>
             </FormControl>
-          </Grid>
-          </Grid>
-
-          <Grid container spacing={5} style={{ paddingTop:10}}>
-        <Grid item xs={true}>
-        <TextField
-              id="amount"
-              name="amount"
+            </Grid>
+            <Grid item xs={3}>
+            <TextField
+              id="sapcode"
+              name="sapcode"
               label="SAP Code"
               size="small"
               variant="standard"
               sx={{ width: 200,marginRight: 2}}
             />
+          </Grid>
+          </Grid>
+
+          <Grid container spacing={5} style={{ paddingTop:10}}>
+          <Grid item xs={3}>
+       
            <TextField
               id="amount"
               name="amount"
@@ -156,14 +194,118 @@ function PatientForm(props: any){
               variant="standard"
               sx={{ width: 200,marginRight: 2}}
             />
+             </Grid>
+            <Grid item xs={3}>
             <FormControlLabel control={<Checkbox />} label="Out Station" />
+            </Grid>
+            <Grid item xs={3}>
             <FormControlLabel control={<Checkbox />} label="Cash Client" />
+            </Grid>
+            <Grid item xs={3}>
             <FormControlLabel control={<Checkbox />} label="Only Mapped Services" />
+            </Grid>
+           
+          </Grid>
+          <Grid container spacing={5} style={{ paddingTop:10}}>
+          <Grid item xs={3}>
             <FormControlLabel control={<Checkbox />} label="Allow Discount ?" />
           </Grid>
+          <Grid item xs={3}>
+            <FormControlLabel control={<Checkbox color="secondary" name="active" value="yes" defaultChecked />} label="Active" />
+            
           </Grid>
-
-        
+          </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+          <Grid container spacing={5} style={{ paddingTop:10}}>
+          <Grid item xs={3}>
+          <FormControl variant="standard">
+              <Autocomplete
+              id="testName"
+              options={hubList}
+              sx={{ width: 200}}
+              renderInput={(params) => <TextField {...params} label="Hub" variant="standard" />}
+            />
+            </FormControl>
+            </Grid>
+          <Grid item xs={3}>
+            <FormControl variant="standard">
+              <Autocomplete
+              id="testName"
+              options={zoneList}
+              sx={{ width: 200,marginRight: 2}}
+              renderInput={(params) => <TextField {...params} label="Zone" variant="standard" />}
+            />
+           </FormControl>
+           </Grid>
+          <Grid item xs={3}>
+           <TextField
+              id="amount"
+              name="amount"
+              label="Route"
+              size="small"
+              variant="standard"
+              sx={{ width: 200,marginRight: 2}}
+            />
+             </Grid>
+             <Grid item xs={3}>
+            <FormControlLabel control={<Checkbox color="secondary" name="active" value="yes" />} label="Invoice" />
+            
+          </Grid>
+          <Grid item xs={3}>
+          <label htmlFor="contained-button-file">Documents&nbsp;
+            <Input accept="image/*" id="contained-button-file" multiple type="file" />
+            <Button variant="contained" component="span">
+              Add Attachment
+            </Button>
+            </label>
+            </Grid>
+            <Grid item xs={3}>
+            <FormControl variant="standard">
+              <InputLabel id="titles-label">Invoice Cycle</InputLabel>
+              <Select
+                labelId="titles-label"
+                id="titles"
+                value={businessType}
+                onChange={(ev) => setBusinessType(ev.target.value)}
+                label="Business Type"
+                style={{width: 200, marginRight:2}}
+                size="small"
+              >
+                <MenuItem value="0">--Select--</MenuItem>
+                <MenuItem value="1">Monthly</MenuItem>
+                <MenuItem value="2">Quaterly</MenuItem>
+                <MenuItem value="2">Annual</MenuItem>
+              </Select>
+            </FormControl>
+            </Grid>
+          </Grid>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+         
+          <Grid container spacing={5} style={{ paddingTop:10}}>
+          <Grid item xs={3}>
+          <TextField
+              id="amount"
+              name="amount"
+              label="Email"
+              size="small"
+              variant="standard"
+              sx={{ width: 200,marginRight: 2}}
+            />
+            </Grid>
+            <Grid item xs={3}>
+          <TextField
+              id="amount"
+              name="amount"
+              label="SMS"
+              size="small"
+              variant="standard"
+              sx={{ width: 200,marginRight: 2}}
+            />
+            </Grid>
+            </Grid>
+            </TabPanel>
       </React.Fragment>
     )
 }
